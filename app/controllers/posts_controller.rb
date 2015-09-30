19d5eaed144e2199f48751cc3782f6f65758edfe
Mateusz <teg34@tlen.pl>
2015-09-30 16:32:11 +0200

    Add preview posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   before_action :authorize, only: [:create, :destroy]
   before_action :check_user, only: [:destroy]
   
+  def show
+    @post = Post.find(params[:id])
+  end
+  
   def create
     @post = current_user.posts.build(post_params)
     
