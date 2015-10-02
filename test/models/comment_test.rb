813a9ba8ad353852d901a6e4e929326a33cf9ecc
Mateusz <teg34@tlen.pl>
2015-10-02 15:56:29 +0200

    Add comment model

---
+++ test/models/comment_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class CommentTest < ActiveSupport::TestCase
+
+  def setup
+    extend ActionDispatch::TestProcess
+    @user = users(:john)
+    @post = posts(:first)
+    @comment =  @post.comments.build(user_id: 1, body: "Great picture!")
+  end
+  
+   test "comment should be valid" do
+      assert @comment.valid?
+   end
+   
+  test "body should be present" do
+    @comment.body = ""
+    assert_not @comment.valid?
+  end
+  
+  test "body should not be too long" do
+    @comment.body = "a" * 401
+    assert_not @comment.valid?
+  end
+  
+end
